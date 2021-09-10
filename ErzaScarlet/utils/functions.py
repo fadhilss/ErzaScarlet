be6bd9640a09b45235e2c5e4384b0a40561ad114
fadhilss <89438065+fadhilss@users.noreply.github.com>
2021-09-10 23:51:58 +0800

    Create functions.py

---
+++ ErzaScarlet/utils/functions.py
@@ -0,0 +1,24 @@
+from asyncio import gather
+from datetime import datetime, timedelta
+from io import BytesIO
+from math import atan2, cos, radians, sin, sqrt
+from os import execvp
+from random import randint
+from re import findall
+from re import sub as re_sub
+from sys import executable
+
+import aiofiles
+import speedtest
+from PIL import Image, ImageDraw, ImageFilter, ImageFont
+from pyrogram.types import Message
+
+from ErzaScarlet import aiohttpsession as aiosession
+from ErzaScarlet.utils.http import get, post
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiosession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
